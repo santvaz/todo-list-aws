ac2eff2c81e6910ba23d41acbbc259ea3e8da40d
santvaz <stawfikvazquez@gmail.com>
2026-02-28 17:00:16 +0100

    todoList.py dynamodb error

--- src/todoList.py
+++ src/todoList.py
@@ -9,13 +9,13 @@ from botocore.exceptions import ClientError
 
 def get_table(dynamodb=None):
     if not dynamodb:
-        URL = os.environ['ENDPOINT_OVERRIDE']
+        URL = os.environ.get('ENDPOINT_OVERRIDE', '')
         if URL:
-            print('URL dynamoDB:'+URL)
-            boto3.client = functools.partial(boto3.client, endpoint_url=URL)
-            boto3.resource = functools.partial(boto3.resource,
-                                               endpoint_url=URL)
-        dynamodb = boto3.resource("dynamodb-dbdbdb")
+            print('URL dynamoDB:' + URL)
+            dynamodb = boto3.resource("dynamodb", endpoint_url=URL)
+        else:
+            dynamodb = boto3.resource("dynamodb")
+            
     # fetch todo from the database
     table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
     return table
